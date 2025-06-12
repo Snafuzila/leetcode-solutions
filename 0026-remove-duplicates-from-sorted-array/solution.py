3253cf736b0fe31115a93650e9d1e6c19097d469
Boaz Attiya <55194933+Snafuzila@users.noreply.github.com>
2025-06-12 21:50:44 +0000

    [LeetCode Sync] Runtime - 4 ms (40.34%), Memory - 13.8 MB (43.96%)

---
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+        
+        k = 1  # Index to place the next unique element
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
