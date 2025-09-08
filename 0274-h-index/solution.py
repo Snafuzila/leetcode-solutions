1392241819a3af782fd904c1921c44912cc3ddd7
Boaz Attiya <55194933+Snafuzila@users.noreply.github.com>
2025-09-08 21:17:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (93.36%)

---
+++ 0274-h-index/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hIndex(self, citations):
+        citations.sort(reverse=True)
+        h = 0
+        
+        for i, c in enumerate(citations):
+            if c >= i + 1:  # check if this paper supports at least h = i+1
+                h = i + 1
+            else:
+                break
+
+        return h
+        
