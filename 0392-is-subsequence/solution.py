3e7b2143b49a13249082cfccad3bff715616285f
Boaz Attiya <55194933+Snafuzila@users.noreply.github.com>
2026-08-23 12:58:25 +0000

    [LeetCode Sync] Runtime - 1 ms (34.63%), Memory - 19.2 MB (65.63%)

---
+++ 0392-is-subsequence/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sindex = 0
+        tindex = 0
+        while sindex < len(s) and tindex< len(t):
+            if t[tindex] == s[sindex]:
+                sindex+=1
+            tindex+=1
+        if sindex==len(s):
+            return True
+        return False
+        
+            
