15225c87c7815cef1bdecf92841eaa638a8103ee
Boaz Attiya <55194933+Snafuzila@users.noreply.github.com>
2026-09-15 10:14:15 +0000

    [LeetCode Sync] Runtime - 49 ms (42.94%), Memory - 24.4 MB (52.83%)

--- 1586-longest-subarray-of-1s-after-deleting-one-element/solution.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/solution.py
@@ -2,20 +2,14 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         zeros = 0
         left = 0
-        longest = 0
-
+        maximum = 0
         for right in range(len(nums)):
             if nums[right] == 0:
-                zeros += 1
-
+                zeros+=1
             while zeros > 1:
                 if nums[left] == 0:
-                    zeros -= 1
-                left += 1
-
-            longest = max(longest, right - left)
-
-        return longest
-
-
-
+                    zeros-=1
+                left+=1
+            maximum = max(maximum, right - left)
+        return maximum
+        
